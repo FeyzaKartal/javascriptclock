c5071dd0baa50df4ae8eaa425cf634e2971ee8cf
FeyzaKartal <feyza-kartal-@hotmail.com>
2023-01-11 14:24:31 +0300

    first commit

---
+++ js/clock.js
@@ -0,0 +1,21 @@
+function showTime() {
+    const days = ["Pazar","Pazartesi","Salı","Çarşamba","Perşembe","Cuma","Cumartesi"];
+    let today = new Date();
+    let h = today.getHours(); // saat gösteren fonksiyon
+    let m = today.getMinutes(); // dakikayı gösteren fonksiyon
+    let s = today.getSeconds(); // saniye'yi gösteren fonksiyon
+    let d = days[today.getDay()]; // diziler içerisindeki değişkeni bağlayıp gün gösteren fonksiyon
+    m = checkTime(m);
+    s = checkTime(s);
+
+    document.querySelector("#myClock").innerHTML = `${h}: ${m}: ${s}: ${d}`;
+    setTimeout(showTime,1000);
+}
+
+showTime();
+function checkTime(i) {
+    if(i<10) {
+        i = "0"+i
+    }
+    return i;
+}
